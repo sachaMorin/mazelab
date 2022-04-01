b0fc69a5f9b288e7cf08505967de2c18b06d62e0
sachaMorin <morin.sacha@gmail.com>
2022-04-01 13:43:09 -0400

    Fix random seed of random shape env
    Also use chanel_axis instead of multichannel.

--- mazelab/generators/random_shape_maze.py
+++ mazelab/generators/random_shape_maze.py
@@ -3,8 +3,9 @@ import numpy as np
 from skimage.draw import random_shapes
 
 
-def random_shape_maze(width, height, max_shapes, max_size, allow_overlap, shape=None):
-    x, _ = random_shapes([height, width], max_shapes, max_size=max_size, multichannel=False, shape=shape, allow_overlap=allow_overlap)
+def random_shape_maze(width, height, max_shapes, max_size, allow_overlap, shape=None, random_state=None):
+    x, _ = random_shapes([height, width], max_shapes, max_size=max_size, channel_axis=None, shape=shape,
+                         allow_overlap=allow_overlap, random_state=random_state)
     
     x[x == 255] = 0
     x[np.nonzero(x)] = 1
